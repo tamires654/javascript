2153d7c54674fbef09db36ba8c32f604ef8b1a0b
tamires654 <148469779+tamires654@users.noreply.github.com>
2025-01-25 11:05:05 -0300

    Add files via upload

---
+++ index.jss.js
@@ -0,0 +1,9 @@
+// Math = built-in object thst provides a 
+//        collection of properties and methods
+
+let x = 3.99;
+let y = 2;
+let z;
+//z = Math.round(x)
+z = Math.floor(x);
+console.log(z);
